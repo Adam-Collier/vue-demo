3d72e36b33c1cfedd79d35cde34aba0bf85c80eb
Adam Collier <adamcollierart@gmail.com>
2018-08-27 16:21:30 +0100

    component created

---
+++ src/components/MyFirstComponent.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Our first component</h1>
+    <h2>This is amazing</h2>
+  </div>
+</template>
--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -1,5 +1,18 @@
+
 <template>
   <div>
     <h1>Hello World! This is a new route</h1>
+    <MyFirstComponent/>
   </div>
 </template>
+
+<script>
+import MyFirstComponent from "@/components/MyFirstComponent";
+
+export default {
+  components: {
+    MyFirstComponent
+  }
+};
+</script>
+
