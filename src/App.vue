785fb3b19f8cfc9ea89ec4f93a5974125eeebf31
Adam Collier <adamcollierart@gmail.com>
2018-08-27 15:56:42 +0100

    added a route

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/hello">Hello</router-link>
     </div>
     <router-view/>
   </div>
 </template>
+
 <style lang="stylus">
 #app
   font-family: 'Avenir', Helvetica, Arial, sans-serif
--- src/router.js
+++ src/router.js
@@ -21,6 +21,12 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/hello",
+      name: "hello",
+      component: () =>
+        import(/* webpackChunkName: "hello" */ "./views/Hello.vue")
     }
   ]
 });
