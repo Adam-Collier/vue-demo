4dd25e0ccbb1b7654e64a7c5b2b347763ac84aa6
Adam Collier <adamcollierart@gmail.com>
2018-08-27 16:51:07 +0100

    reactive component created

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -3,15 +3,23 @@
   <div>
     <h1>Hello World! This is a new route</h1>
     <MyFirstComponent/>
+    <ButtonComponent/>
   </div>
 </template>
 
 <script>
 import MyFirstComponent from "@/components/MyFirstComponent";
+import ButtonComponent from "@/components/ButtonComponent";
 
 export default {
+  data: function() {
+    return {
+      count: 0
+    };
+  },
   components: {
-    MyFirstComponent
+    MyFirstComponent,
+    ButtonComponent
   }
 };
 </script>
